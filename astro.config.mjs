36f496eee27b2c69a6c4a5a874577e6d7d91660e
Jose Manuel Castro Balbín <jcastroba@unal.edu.co>
2025-12-24 15:42:58 -0500

    Clean astro config and force vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import dotenv from 'dotenv';
 import vercel from '@astrojs/vercel/serverless';
-
-// Cargar variables de entorno desde .env
-dotenv.config();
 
 export default defineConfig({
   integrations: [
